340793b33851c72b8b2ae290e57f7304ca134082
Jean Pierre Wenzel <jeanpierre.wenzel@gmail.com>
2009-11-28 17:20:43 +0100

    First check-in of rewrite.lua

---
+++ rewrite.lua
@@ -0,0 +1,85 @@
+--[[
+rewrite.lua
+-------------------------------------------------------------------------------
+Rewrite rules for a Wordpress 2.x installation on top of a lighttpd web server
+
+Written by Giovanni Intini, Jean Pierre Wenzel <jpwenzel@gmx.net>
+
+This file comes in very handy if your using the following Wordpress Plugins:
+* wp-super-cache (http://ocaoimh.ie/wp-super-cache/)
+* WP-touch (http://bravenewcode.com/wptouch)
+
+This LUA script is based on the great work of Giovanni Intini:
+http://tempe.st/2008/05/lightning-speed-wordpress-with-lighttpd-and-supercache-part-ii/
+-------------------------------------------------------------------------------
+--]]
+
+-- ----------------------------------------------------------------------------
+-- CONFIGURATION
+enableUserAgentCheck = true
+-- ----------------------------------------------------------------------------
+
+function serve_html(cached_page)
+  if (lighty.stat(cached_page)) then
+    lighty.env["physical.path"] = cached_page
+    return true
+  else
+    return false
+  end
+end
+ 
+function serve_gzip(cached_page)
+  if (lighty.stat(cached_page .. ".gz")) then
+    lighty.header["Content-Encoding"] = "gzip"
+    lighty.header["Content-Type"] = ""
+    lighty.env["physical.path"] = cached_page .. ".gz"
+    return true
+  else
+    return false
+  end
+end
+
+attr = lighty.stat(lighty.env["physical.path"])
+ 
+if (not attr) then
+  lighty.env["uri.path"] = "/index.php"
+  lighty.env["physical.rel-path"] = lighty.env["uri.path"]
+  lighty.env["physical.path"] = lighty.env["physical.doc-root"] .. lighty.env["physical.rel-path"]
+  
+  -- Sending a HTTP query? => no caching
+  query_condition = not (lighty.env["uri.query"] and string.find(lighty.env["uri.query"], ".*s=.*"))
+  
+  -- Have a cookie? => no caching
+  user_cookie = lighty.request["Cookie"] or ""
+  cookie_condition = not (string.find(user_cookie, ".*comment_author.*") or string.find(user_cookie, ".*wordpress.*") or string.find(user_cookie, ".*wp-postpass_.*"))
+  
+  if (not enableUserAgentCheck) then
+    sendCachedFile = true
+  else
+    -- Check if request comes from a mobile device or bot => no caching than, either.
+    local userAgentsNoCaching = { "bot", "ia_archive", "slurp", "crawl", "spider", "linkbot", "linklift", "postlift", "iphone", "ipod", "android", "cupcake", "webos", "incognito", "webmate", "opera mini", "blackberry", "symbian", "series60", "nokia" }
+    userAgent = lighty.request["User-Agent"]
+    if (nil == userAgent) then
+      sendCachedFile = true
+    else
+      userAgent = string.lower(userAgent)
+      for i, v in ipairs(userAgentsNoCaching) do
+        if string.find(v, userAgent) then
+          sendCachedFile = false
+  	      break
+        end  
+      end
+    end
+  end
+  
+  if (query_condition and cookie_condition and sendCachedFile) then
+    accept_encoding = lighty.request["Accept-Encoding"] or "no_acceptance"
+    cached_page = lighty.env["physical.doc-root"] .. "/wp-content/cache/supercache/" .. lighty.request["Host"] .. lighty.env["request.uri"] .. "/index.html"
+    cached_page = string.gsub(cached_page, "//", "/")
+    if (string.find(accept_encoding, "gzip")) then
+      if not serve_gzip(cached_page) then serve_html(cached_page) end
+    else
+      serve_html(cached_page)
+    end
+  end
+end
