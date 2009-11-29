fb3805bf759c940d52c8cb4854589706ce58e0d4
Jean Pierre Wenzel <jeanpierre.wenzel@gmail.com>
2009-11-29 19:21:14 +0100

    Updated User Agents

--- rewrite.lua
+++ rewrite.lua
@@ -72,7 +72,7 @@ if (not attr) then
     sendCachedFile = true
   else
     -- Check if request comes from a mobile device or bot => no caching than, either.
-    local userAgentsNoCaching = { "bot", "ia_archive", "slurp", "crawl", "spider", "linkbot", "linklift", "postlift", "iphone", "ipod", "android", "cupcake", "webos", "incognito", "webmate", "opera mini", "blackberry", "symbian", "series60", "nokia" }
+    local userAgentsNoCaching = { "bot", "ia_archive", "slurp", "crawl", "spider", "linkbot", "iphone", "ipod", "android", "cupcake", "webos", "incognito", "webmate", "opera mini", "blackberry", "symbian", "series60", "nokia", "samsung" }
     userAgent = lighty.request["User-Agent"]
     if (nil == userAgent) then
       sendCachedFile = true
